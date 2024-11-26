6e7cf50a2986720a38b877f58d8b50878b18784d
guru738 <gurukirankj0@gmail.com>
2024-11-26 15:01:48 +0530

    Create while loop.c

---
+++ while loop.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+    int n, i = 1; sum=0
+    printf("Enter the value of N: ");
+    scanf("%d", &n);
+    while (i <= n) {
+      sum+=i;
+        i++:
+    }
+    printf("sum of first %d natural number is %d\n",n,sum);
+    return 0;
+}
